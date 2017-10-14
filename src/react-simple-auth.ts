2fb64be38ddf8818eb5244fd53d786ddbaa8571c
Matt Mazzola <mattmazzola@live.com>
2017-10-14 15:53:22 -0700

    Add react-simple-auth service with tests. Reduce branch coverage to 84 due to optional parameters.

--- src/react-simple-auth.ts
+++ src/react-simple-auth.ts
@@ -1,4 +1,148 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+const sessionKey = 'session'
+
+export interface IProvider<T> {
+  buildAuthorizeUrl(): string
+  extractError(redirectUrl: string): Error | undefined
+  extractSession(redirectUrl: string): T
+  validateSession(session: T): boolean
+  getAccessToken(session: T, resourceId: string): string
+  getSignOutUrl(redirectUrl: string): string
+}
+
+export interface IAuthenticationService {
+  acquireTokenAsync<T>(
+    provider: IProvider<T>,
+    storage?: Storage,
+    localWindow?: Window
+  ): Promise<T>
+  restoreSession<T>(provider: IProvider<T>, storage?: Storage): T | undefined
+  invalidateSession(storage?: Storage): void
+  getAccessToken<T>(
+    provider: IProvider<T>,
+    resourceId: string,
+    storage?: Storage
+  ): string
+}
+
+export const service: IAuthenticationService = {
+  acquireTokenAsync<T>(
+    provider: IProvider<T>,
+    storage: Storage = window.localStorage,
+    localWindow: Window = window
+  ): Promise<T> {
+    // Create unique request key
+    const requestKey = `react-simple-auth-request-key-${guid()}`
+
+    // Create new window set to authorize url, with unique request key, and centered options
+    const [width, height] = [500, 500]
+    const windowOptions = {
+      width,
+      height,
+      left:
+        Math.floor(screen.width / 2 - width / 2) +
+        ((screen as any).availLeft || 0),
+      top: Math.floor(screen.height / 2 - height / 2)
+    }
+
+    const oauthAuthorizeUrl = provider.buildAuthorizeUrl()
+    const windowOptionString = Object.entries(windowOptions)
+      .map(([key, value]) => `${key}=${value}`)
+      .join(',')
+    const loginWindow = localWindow.open(
+      oauthAuthorizeUrl,
+      requestKey,
+      windowOptionString
+    )
+
+    return new Promise<any>((resolve, reject) => {
+      // Poll for when the is closed
+      const checkWindow = (loginWindow: Window) => {
+        // If window is still open check again later
+        if (!loginWindow.closed) {
+          setTimeout(() => checkWindow(loginWindow), 100)
+          return
+        }
+
+        const redirectUrl = storage.getItem(requestKey)
+        storage.removeItem(requestKey)
+
+        // Window was closed, but never reached the redirect.html due to user closing window or network error during authentication
+        if (typeof redirectUrl !== 'string' || redirectUrl.length === 0) {
+          reject(
+            new Error(
+              `React Simple Auth: Login window was closed by the user or authentication was incomplete and never reached final redirect page.`
+            )
+          )
+          return
+        }
+
+        // Window was closed, and reached the redirect.html; however there still might have been error during authentication, check url
+        const error = provider.extractError(redirectUrl)
+        if (error) {
+          reject(error)
+          return
+        }
+
+        // Window was closed, reached redirect.html and correctly added tokens to the url
+        const session = provider.extractSession(redirectUrl)
+        storage.setItem(sessionKey, JSON.stringify(session))
+        resolve(session)
+      }
+
+      checkWindow(loginWindow)
+    })
+  },
+
+  restoreSession<T>(
+    provider: IProvider<T>,
+    storage: Storage = window.localStorage
+  ): T | undefined {
+    const sessionString = storage.getItem(sessionKey)
+    if (typeof sessionString !== 'string' || sessionString.length === 0) {
+      return undefined
+    }
+
+    const session: T = JSON.parse(sessionString)
+
+    if (!provider.validateSession(session)) {
+      storage.removeItem(sessionKey)
+      return undefined
+    }
+
+    return session
+  },
+
+  invalidateSession(storage: Storage = window.localStorage): void {
+    storage.removeItem(sessionKey)
+  },
+
+  getAccessToken<T>(
+    provider: IProvider<T>,
+    resourceId: string,
+    storage: Storage = window.localStorage
+  ): string {
+    const sessionString = storage.getItem(sessionKey)
+    if (typeof sessionString !== 'string' || sessionString.length === 0) {
+      throw new Error(
+        `You attempted to get access token for resource id: ${resourceId} from the session but the session did not exist`
+      )
+    }
+
+    const session: T = JSON.parse(sessionString)
+
+    return provider.getAccessToken(session, resourceId)
+  }
+}
+
+export default service
+
+function guid(): string {
+  var d = new Date().getTime()
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(
+    c: string
+  ) {
+    var r = ((d + Math.random() * 16) % 16) | 0
+    d = Math.floor(d / 16)
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
+}
--- test/react-simple-auth.test.ts
+++ test/react-simple-auth.test.ts
@@ -1,14 +1,228 @@
-import DummyClass from '../src/react-simple-auth'
+import RSA, { IProvider } from '../src/react-simple-auth'
 
 /**
- * Dummy test
+ * Authentication Service Tests
  */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+describe('Authentication Service', () => {
+  const providerMocks: { [x: string]: jest.Mock<any> } = {
+    buildAuthorizeUrl: jest.fn(),
+    extractError: jest.fn(),
+    extractSession: jest.fn(),
+    validateSession: jest.fn(),
+    getAccessToken: jest.fn(),
+    getSignOutUrl: jest.fn()
+  }
+
+  const storageMocks: { [x: string]: jest.Mock<any> } = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn()
+  }
+
+  const windowMocks: { [x: string]: jest.Mock<any> } = {
+    open: jest.fn()
+  }
+
+  const mockProvider: IProvider<{}> = providerMocks as any
+  const mockStorage: Storage = storageMocks as any
+  const mockWindow: Window = windowMocks as any
+
+  beforeEach(() => {
+    jest.resetAllMocks()
   })
 
-  it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  describe('acquireTokenAsync', () => {
+    it('should call provider.buildAuthorizeUrl', async () => {
+      // Arrange
+      windowMocks.open.mockImplementation((url, requestKey, options) => {
+        storageMocks.getItem.mockReturnValue('fake-redirect-url')
+        return { closed: true }
+      })
+
+      // Act
+      const session = await RSA.acquireTokenAsync(
+        mockProvider,
+        mockStorage,
+        mockWindow
+      )
+
+      // Assert
+      expect(providerMocks.buildAuthorizeUrl.mock.calls.length).toBe(1)
+    })
+
+    it('should throw error if storage for request key was undefined or empty after window was closed', async () => {
+      // Arrange
+      windowMocks.open.mockReturnValue({ closed: true })
+
+      // Act
+      try {
+        await RSA.acquireTokenAsync(mockProvider, mockStorage, mockWindow)
+      } catch (e) {
+        // Assert
+        expect(() => {
+          throw e
+        }).toThrowError()
+      }
+    })
+
+    it('should call provider.extractError with redirectUrl and reject if truthy', async () => {
+      // Arrange
+      const testRedirectUrl = 'fake-redirect-url'
+      windowMocks.open.mockImplementation((url, requestKey, options) => {
+        storageMocks.getItem.mockReturnValue(testRedirectUrl)
+        return { closed: true }
+      })
+
+      providerMocks.extractError.mockReturnValue(
+        new Error('invalid credentials')
+      )
+
+      // Act
+      try {
+        await RSA.acquireTokenAsync(mockProvider, mockStorage, mockWindow)
+      } catch (e) {
+        // Assert
+        expect(providerMocks.extractError.mock.calls.length).toBe(1)
+        const [calledRedirectUrl] = providerMocks.extractError.mock.calls[0]
+        expect(calledRedirectUrl).toEqual(testRedirectUrl)
+        expect(() => {
+          throw e
+        }).toThrowError()
+      }
+    })
+
+    it('should call provider.extractSession with redirectUrl save as session and return value', async () => {
+      // Arrange
+      const testRedirectUrl = 'fake-redirect-url'
+      const testSession = { foo: 'bar' }
+      windowMocks.open.mockImplementation((url, requestKey, options) => {
+        storageMocks.getItem.mockReturnValue(testRedirectUrl)
+        return { closed: true }
+      })
+
+      providerMocks.extractError.mockReturnValue(undefined)
+      providerMocks.extractSession.mockReturnValue(testSession)
+
+      // Act
+      const session = await RSA.acquireTokenAsync(
+        mockProvider,
+        mockStorage,
+        mockWindow
+      )
+
+      // Assert
+      expect(providerMocks.extractSession.mock.calls.length).toBe(1)
+      const [calledRedirectUrl] = providerMocks.extractSession.mock.calls[0]
+      expect(calledRedirectUrl).toEqual(testRedirectUrl)
+      expect(session).toBe(testSession)
+    })
+
+    it('will wait for window to be closed before', async () => {
+      // Arrange
+      const testRedirectUrl = 'fake-redirect-url'
+      const testSession = { foo: 'bar' }
+      const mockOpenedWindow = { name: 'testWindow', closed: false }
+      windowMocks.open.mockImplementation((url, requestKey, options) => {
+        storageMocks.getItem.mockReturnValue(testRedirectUrl)
+        setTimeout(() => {
+          mockOpenedWindow.closed = true
+        }, 500)
+        return mockOpenedWindow
+      })
+
+      providerMocks.extractError.mockReturnValue(undefined)
+      providerMocks.extractSession.mockReturnValue(testSession)
+
+      // Act
+      const session = await RSA.acquireTokenAsync(
+        mockProvider,
+        mockStorage,
+        mockWindow
+      )
+
+      // Assert
+      expect(providerMocks.extractSession.mock.calls.length).toBe(1)
+      expect(session).toBe(testSession)
+    })
+  })
+
+  describe('restoreSession', () => {
+    it('should return undefined if session does not exist', () => {
+      // Arrange
+
+      // Act
+      const session = RSA.restoreSession(mockProvider, mockStorage)
+
+      // Assert
+      expect(session).toBeUndefined()
+    })
+
+    it('should return undefined if session exists but is invalid', () => {
+      // Arrange
+      storageMocks.getItem.mockReturnValue('{}')
+      providerMocks.validateSession.mockReturnValue(false)
+
+      // Act
+      const session = RSA.restoreSession(mockProvider, mockStorage)
+
+      // Assert
+      expect(session).toBeUndefined()
+    })
+
+    it('should return session if it is defined and valid', () => {
+      // Arrange
+      storageMocks.getItem.mockReturnValue('{}')
+      providerMocks.validateSession.mockReturnValue(true)
+
+      // Act
+      const session = RSA.restoreSession(mockProvider, mockStorage)
+
+      // Assert
+      expect(session).toEqual({})
+    })
+  })
+
+  describe('invalidateSession', () => {
+    it('should remove the session from storage', () => {
+      // Arrange
+
+      // Act
+      RSA.invalidateSession(mockStorage)
+
+      // Assert
+      expect(storageMocks.removeItem.mock.calls.length).toBe(1)
+    })
+  })
+
+  describe('getAccessToken', () => {
+    it('should throw error if session does not exist', () => {
+      // Arrange
+      storageMocks.getItem.mockReturnValue(undefined)
+
+      // Act
+      const action = () => RSA.getAccessToken(mockProvider, '', mockStorage)
+
+      // Assert
+      expect(action).toThrowError()
+    })
+
+    it('should call provider.getAccessToken with session and resourceId and return value', () => {
+      // Arrange
+      const testSession: any = { foo: 'bar' }
+      const testAccessToken = 'test-access-token'
+      storageMocks.getItem.mockReturnValue(JSON.stringify(testSession))
+      providerMocks.getAccessToken.mockReturnValue(testAccessToken)
+
+      // Act
+      const accessToken = RSA.getAccessToken(mockProvider, '', mockStorage)
+
+      // Assert
+      expect(providerMocks.getAccessToken.mock.calls.length).toBe(1)
+      const [session, resourceId] = providerMocks.getAccessToken.mock.calls[0]
+      expect(session).toEqual(testSession)
+      expect(resourceId).toEqual('')
+      expect(accessToken).toEqual(testAccessToken)
+    })
   })
 })
